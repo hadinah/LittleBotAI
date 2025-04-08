4daf1bffbc30fc8814079f2f1f041755b7d827c0
Hadin AH <libapayapura+git@gmail.com>
2025-04-09 03:18:06 +0530

    Create config.py
    Signed-off-by: Hadin AH <libapayapura+git@gmail.com>

---
+++ src/VoiceAI/config.py
@@ -0,0 +1,20 @@
+'''
+STRICTLY REDACTED,
+Fill in you're own API and IP's to proceed
+'''
+
+GEMINI_CONFIG = {
+    "api_key": "Your API",
+    "model": "AI Model",
+    "system_instruction": (
+        "INSTRUCTIONS FOR AI"
+    ),
+    "streaming": False,
+    "temperature": 0.7,
+    "top_p": 1.0,
+    "top_k": 40,
+    "tts_speed": 1.3  # 1 = normal, >1 = faster
+}
+
+BROKER_IP = "Subscriber IP"
+TOPIC = "Topic IP"
