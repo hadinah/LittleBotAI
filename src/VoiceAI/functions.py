19b06ee6ad60ff4a762b33b5b6975e65335021e2
Hadin AH <libapayapura+git@gmail.com>
2025-04-09 03:19:28 +0530

    Add files via upload
    Signed-off-by: Hadin AH <libapayapura+git@gmail.com>

---
+++ src/VoiceAI/functions.py
@@ -0,0 +1,19 @@
+import os
+#from mqtt_handler import publish
+
+'''
+ADD MORE FEATURES
+'''
+
+def led(state: bool) -> None:
+    if state:
+        print('LED Turned On!!')
+        #publish('on')
+        #easygui.msgbox("LED Turned On!!", title="Function Output")
+    else:
+        print('LED Turned Off!')
+        #publish('off')
+        #easygui.msgbox('LED Turned Off!', title="Function Output")
+
+def run_command(*args, **kwargs) -> None:
+    exec(*args, **kwargs)
